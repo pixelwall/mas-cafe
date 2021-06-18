a6accd9478de5e7d5a6c27afa3b65c3628adb05d
Juan Manuel Villacorta Campos <37244056+juanvillacortac@users.noreply.github.com>
2021-06-18 17:09:48 -0400

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { SnipcartContext } from '@/components/snipcart'
+import '@/styles/app.css'
+import '@/styles/icons.css'
+import PageLayout, { GetLayoutProps } from '@/components/page-layout'
+
+import type { AppProps } from 'next/app'
+import { useMemo } from 'react'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const layoutProps = useMemo<GetLayoutProps>(() => {
+    return (Component as any).getLayoutProps
+      ? (Component as any).getLayoutProps(pageProps)
+      : (pageProps) => pageProps 
+  }, [(Component as any).getLayoutProps, pageProps])
+  return (
+    <SnipcartContext>
+      <PageLayout {...pageProps} {...layoutProps}>
+        <Component {...pageProps} />
+      </PageLayout>
+    </SnipcartContext>
+  )
+}
+
+export default App
---
+++ pages/index.tsx
@@ -0,0 +1,1 @@
+export { default } from '@w3/pages/home'
