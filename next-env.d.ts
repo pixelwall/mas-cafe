603c56566cc16c987a3ead611990eedc91bbec7b
antossd1411 <86059197+antossd1411@users.noreply.github.com>
2021-06-28 20:55:43 -0400

    Delete next-env.d.ts

--- next-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-
-interface SvgrComponent extends React.FC<React.SVGAttributes<SVGElement>> {}
-
-declare module '*.svg' {
-  const value: SvgrComponent
-  export default value
-}
